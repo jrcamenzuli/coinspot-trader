a73c78508e4a1c79517f3c84cc0b77a2b2593a2e
Jonathan Camenzuli <jrcamenzuli@gmail.com>
2023-04-19 19:55:24 +1000

    progress

--- run-publisher.ps1
+++ run-publisher.ps1
@@ -1,1 +1,1 @@
-docker run --rm --name coinspot-trader -p 8080:8080 -e COINSPOT_KEY=$env:COINSPOT_KEY -e COINSPOT_SECRET=$env:COINSPOT_SECRET -d coinspot-trader ./app -mode publisher
+docker login -u jrcamenzuli -p $env:DOCKER_TOKEN_GITHUB ghcr.io ; docker run --restart always --name coinspot-trader -p 10000:8080 -e COINSPOT_KEY=$env:COINSPOT_KEY -e COINSPOT_SECRET=$env:COINSPOT_SECRET -d ghcr.io/jrcamenzuli/coinspot-trader ./app -mode publisher
--- subscriber/subscriber.go
+++ subscriber/subscriber.go
@@ -1,58 +1,17 @@
 package subscriber
 
 import (
-	"fmt"
 	"log"
-
-	amqp "github.com/rabbitmq/amqp091-go"
+	"net/http"
 )
 
 func Start() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	fs := http.FileServer(http.Dir("subscriber/frontend"))
+	http.Handle("/", fs)
+
+	log.Println("The frontend is listening on :8081...")
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
+		log.Fatal(err)
 	}
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatalf("Failed to open a channel: %s", err)
-	}
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // queue name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	if err != nil {
-		log.Fatalf("Failed to declare a queue: %s", err)
-	}
-
-	msgs, err := ch.Consume(
-		q.Name, // queue name
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	if err != nil {
-		log.Fatalf("Failed to register a consumer: %s", err)
-	}
-
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-			fmt.Printf("Received message: %s", d.Body)
-		}
-	}()
-
-	fmt.Println("Waiting for messages. To exit press CTRL+C")
-	<-forever
 }
