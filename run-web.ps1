9f433ec1554673d297bcfa06a6e57118fdbb1981
Jonathan Camenzuli <jrcamenzuli@gmail.com>
2023-04-20 18:58:42 +1000

    progress

---
+++ run-web.ps1
@@ -0,0 +1,1 @@
+docker login -u jrcamenzuli -p $env:DOCKER_TOKEN_GITHUB ghcr.io ; docker run --restart always --pull always --name coinspot-trader -p 10000:8080 -e COINSPOT_KEY=$env:COINSPOT_KEY -e COINSPOT_SECRET=$env:COINSPOT_SECRET -d ghcr.io/jrcamenzuli/coinspot-trader ./app -mode web
