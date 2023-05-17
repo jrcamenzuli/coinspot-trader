27f44d4599cfd2141b3b1133b3d08ca4e0a89fdc
Jonathan Camenzuli <jrcamenzuli@gmail.com>
2023-05-17 22:47:26 +1000

    progress

--- src/coinspot_trader.clj
+++ src/coinspot_trader.clj
@@ -1,18 +1,12 @@
 (ns coinspot-trader
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clj-http.client :as client]
-            [cheshire.core :as json]))
-(defn get-snapshot [t]
-  (let
-    [
-     url "http://192.168.0.40:10000/query"
-     response (client/get url {:as :reader :accept :json :query-params {"t" t}})
-     ]
-    (with-open [reader (:body response)]  ; closes the underlying connection when we're done
-      (let [snapshots (json/parse-stream reader true)]
-        ; You must perform all reads from the stream inside `with-open`,
-        ; any , any lazy
-        (doall (for [snapshot snapshots]
-                 snapshot))))))
-(get-snapshot "1684323847")
+  (:require
+    [clj-http.client :as client]
+    [clj-time.coerce :as coerce]))
+
+(defn get-snapshots [t]
+  (let [url "http://192.168.0.40:10000/query"
+        response (client/get url {:as :json, :query-params {"t" t}})
+        snapshots (mapv #(update % :Time coerce/from-string) (:body response))]
+    snapshots))
+
+(get-snapshots "1684323847")
